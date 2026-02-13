f14e2ebce09bd4e6acb68a0d8da211a5c620690f
harshit bansal <code.crypt2020@gmail.com>
2026-02-13 18:19:21 +0530

    added file structure

---
+++ lib/Home/Screens/home_main.dart
@@ -0,0 +1,19 @@
+// import 'package:flutter/material.dart';
+
+// class screen_home_main extends StatelessWidget {
+
+//   const screen_home_main({super.key});
+
+//   @override
+//   State<screen_home_main> createState() => _AttendeeHomePageState();
+// }
+import 'package:flutter/material.dart';
+
+class home_main extends StatelessWidget {
+  const home_main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
